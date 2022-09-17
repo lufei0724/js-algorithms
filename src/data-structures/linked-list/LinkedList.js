4991d99fd8b9a18f9ea9e235cee4854b4236c8a2
Matt Lu <matthew.lu@myob.com>
2022-09-17 17:12:16 +1000

    Add delete method in LinkedList

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -32,4 +32,47 @@ export default class LinkedList {
 
     return this;
   }
+
+  prepend(value) {
+    const node = new LinkedListNode(value);
+    node.next = this.head;
+    this.head = node;
+
+    if (!this.tail) {
+      this.tail = node;
+    }
+
+    return this;
+  }
+
+  delete(value) {
+    while (this.head && this.head.value === value) {
+      this.deleteHead();
+    }
+
+    let node = this.head;
+    if (node !== null) {
+      while (node.next) {
+        if (node.next.value === value) {
+          node.next = node.next.next;
+        } else {
+          node = node.next;
+        }
+      }
+    }
+
+    this.tail = node;
+    return this;
+  }
+
+  deleteHead() {
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return this;
+  }
 }
--- src/data-structures/linked-list/__test__/LinkedList.test.js
+++ src/data-structures/linked-list/__test__/LinkedList.test.js
@@ -15,6 +15,187 @@ describe('LinkedList', () => {
     linkedList.append(1);
     linkedList.append(2);
 
+    expect(linkedList.head.toString()).toBe('1');
+    expect(linkedList.tail.toString()).toBe('2');
     expect(linkedList.toString()).toBe('1,2');
+  });
+
+  it('should prepend node to linked list', () => {
+    const linkedList = new LinkedList();
+
+    expect(linkedList.head).toBe(null);
+    expect(linkedList.tail).toBe(null);
+
+    linkedList.prepend(1);
+    linkedList.prepend(2);
+
+    expect(linkedList.head.toString()).toBe('2');
+    expect(linkedList.tail.toString()).toBe('1');
+    expect(linkedList.toString()).toBe('2,1');
+  });
+
+  describe('when delete head', () => {
+    it('should delete head from linked list', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.prepend(1);
+      linkedList.prepend(2);
+      linkedList.append(3);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,1,3');
+
+      linkedList.deleteHead();
+      expect(linkedList.head.toString()).toBe('1');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('1,3');
+    });
+
+    it('should return empty linked list given the linked list only has one node', () => {
+      const linkedList = new LinkedList();
+      linkedList.append(1);
+      expect(linkedList.head.toString()).toBe('1');
+      expect(linkedList.tail.toString()).toBe('1');
+      expect(linkedList.toString()).toBe('1');
+
+      linkedList.deleteHead();
+      expect(linkedList.head.toString()).toBe('');
+      expect(linkedList.tail.toString()).toBe('');
+      expect(linkedList.toString()).toBe('');
+    });
+
+    it('should return empty linked list given the linked list is empty', () => {
+      const linkedList = new LinkedList();
+      expect(linkedList.head.toString()).toBe('');
+      expect(linkedList.tail.toString()).toBe('');
+      expect(linkedList.toString()).toBe('');
+
+      linkedList.deleteHead();
+      expect(linkedList.head.toString()).toBe('');
+      expect(linkedList.tail.toString()).toBe('');
+      expect(linkedList.toString()).toBe('');
+    });
+  });
+
+  describe('when delete node', () => {
+    it('should delete node from linked list', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.prepend(1);
+      linkedList.prepend(2);
+      linkedList.append(3);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,1,3');
+
+      linkedList.delete(1);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,3');
+    });
+
+    it('should delete head from linked list given the head is equal the value to be deleted', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.prepend(1);
+      linkedList.prepend(2);
+      linkedList.append(3);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,1,3');
+
+      linkedList.delete(2);
+      expect(linkedList.head.toString()).toBe('1');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('1,3');
+    });
+
+    it('should delete tail given the tail is equal the value to be deleted', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.prepend(1);
+      linkedList.prepend(2);
+      linkedList.append(3);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,1,3');
+
+      linkedList.delete(3);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('1');
+      expect(linkedList.toString()).toBe('2,1');
+    });
+
+    it('should return empty list given the linked list only has one node and equal the value to be deleted', () => {
+      const linkedList = new LinkedList();
+      linkedList.prepend(1);
+      expect(linkedList.head.toString()).toBe('1');
+      expect(linkedList.tail.toString()).toBe('1');
+      expect(linkedList.toString()).toBe('1');
+
+      linkedList.delete(1);
+      expect(linkedList.head.toString()).toBe('');
+      expect(linkedList.tail.toString()).toBe('');
+      expect(linkedList.toString()).toBe('');
+    });
+
+    it('should return the same list given the linked list has no node equal the value to be deleted', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.prepend(1);
+      linkedList.prepend(2);
+      linkedList.append(3);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,1,3');
+
+      linkedList.delete(4);
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('3');
+      expect(linkedList.toString()).toBe('2,1,3');
+    });
+
+    it('should delete node by value from linked list', () => {
+      const linkedList = new LinkedList();
+
+      linkedList.append(1);
+      linkedList.append(1);
+      linkedList.append(2);
+      linkedList.append(3);
+      linkedList.append(3);
+      linkedList.append(3);
+      linkedList.append(4);
+      linkedList.append(5);
+
+      expect(linkedList.head.toString()).toBe('1');
+      expect(linkedList.tail.toString()).toBe('5');
+
+      linkedList.delete(3);
+      expect(linkedList.toString()).toBe('1,1,2,4,5');
+
+      linkedList.delete(3);
+      expect(linkedList.toString()).toBe('1,1,2,4,5');
+
+      linkedList.delete(1);
+      expect(linkedList.toString()).toBe('2,4,5');
+
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('5');
+
+      linkedList.delete(5);
+      expect(linkedList.toString()).toBe('2,4');
+
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('4');
+
+      linkedList.delete(4);
+      expect(linkedList.toString()).toBe('2');
+
+      expect(linkedList.head.toString()).toBe('2');
+      expect(linkedList.tail.toString()).toBe('2');
+
+      linkedList.delete(2);
+      expect(linkedList.toString()).toBe('');
+    });
   });
 });
