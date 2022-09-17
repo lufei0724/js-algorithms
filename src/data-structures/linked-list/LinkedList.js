2404a9c43e368108738e9f9ec9b6501848491d61
Matt Lu <matthew.lu@myob.com>
2022-09-17 19:32:19 +1000

    Add insert method in LinkedList

--- src/data-structures/linked-list/LinkedList.js
+++ src/data-structures/linked-list/LinkedList.js
@@ -77,4 +77,34 @@ export default class LinkedList {
 
     return this;
   }
+
+  insert(value, rawIndex) {
+    const index = rawIndex < 0 ? 0 : rawIndex;
+    if (index === 0) {
+      this.prepend(value);
+      return this;
+    }
+
+    const newNode = new LinkedListNode(value);
+    let currentNode = this.head;
+    if (!currentNode) {
+      this.prepend(value);
+      return this;
+    }
+
+    let count = 1;
+    while (currentNode.next) {
+      if (count === index) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+        break;
+      }
+      currentNode = currentNode.next;
+      count += 1;
+    }
+    if (count < index) {
+      this.append(newNode);
+    }
+    return this;
+  }
 }
